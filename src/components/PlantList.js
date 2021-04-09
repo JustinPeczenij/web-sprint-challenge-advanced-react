a44134742291ad486d48cf464f99e9550822339b
Justin Peczenij <justinpeczenij@gmail.com>
2021-04-09 11:34:12 -0400

    added plants array as state to PlantList class, set plant state on axios call promise

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,14 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
-
+  constructor() {
+    super();
+    this.state = {
+      plants: []
+    }
+  }
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
@@ -12,7 +17,10 @@ export default class PlantList extends Component {
   componentDidMount() {
     axios.get('http://localhost:3333/plants')
       .then(res => {
-        console.log(res);
+        this.setState({
+          plants: res.data
+        })
+        console.log(this.state);
       });
   }
 
